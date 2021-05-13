0868b161afbc3b4a98e54dffeb66790ac73e5186
hrithiklodha <8655130-hrithiklodha@users.noreply.gitlab.com>
2021-05-13 18:58:44 +0000

    Linting errors fix

--- contracts/lib/DateTimeLibrary.sol
+++ contracts/lib/DateTimeLibrary.sol
@@ -28,18 +28,18 @@ pragma solidity 0.7.6;
 
 library BokkyPooBahsDateTimeLibrary {
 
-    uint constant SECONDS_PER_DAY = 24 * 60 * 60;
-    uint constant SECONDS_PER_HOUR = 60 * 60;
-    uint constant SECONDS_PER_MINUTE = 60;
-    int constant OFFSET19700101 = 2440588;
+    uint private constant SECONDS_PER_DAY = 24 * 60 * 60;
+    uint private constant SECONDS_PER_HOUR = 60 * 60;
+    uint private constant SECONDS_PER_MINUTE = 60;
+    int private constant OFFSET19700101 = 2440588;
 
-    uint constant DOW_MON = 1;
-    uint constant DOW_TUE = 2;
-    uint constant DOW_WED = 3;
-    uint constant DOW_THU = 4;
-    uint constant DOW_FRI = 5;
-    uint constant DOW_SAT = 6;
-    uint constant DOW_SUN = 7;
+    uint private constant DOW_MON = 1;
+    uint private constant DOW_TUE = 2;
+    uint private constant DOW_WED = 3;
+    uint private constant DOW_THU = 4;
+    uint private constant DOW_FRI = 5;
+    uint private constant DOW_SAT = 6;
+    uint private constant DOW_SUN = 7;
 
     // ------------------------------------------------------------------------
     // Calculate the number of days from 1970/01/01 to year/month/day using
@@ -76,30 +76,31 @@ library BokkyPooBahsDateTimeLibrary {
     //   http://aa.usno.navy.mil/faq/docs/JD_Formula.php
     // and adding the offset 2440588 so that 1970/01/01 is day 0
     //
-    // int L = days + 68569 + offset
-    // int N = 4 * L / 146097
-    // L = L - (146097 * N + 3) / 4
-    // year = 4000 * (L + 1) / 1461001
-    // L = L - 1461 * year / 4 + 31
-    // month = 80 * L / 2447
-    // dd = L - 2447 * month / 80
-    // L = month / 11
-    // month = month + 2 - 12 * L
-    // year = 100 * (N - 49) + year + L
+    // int tempL = days + 68569 + offset
+    // int tempN = 4 * tempL / 146097
+    // tempL = tempL - (146097 * tempN + 3) / 4
+    // year = 4000 * (tempL + 1) / 1461001
+    // tempL = tempL - 1461 * year / 4 + 31
+    // month = 80 * tempL / 2447
+    // dd = tempL - 2447 * month / 80
+    // tempL = month / 11
+    // month = month + 2 - 12 * tempL
+    // year = 100 * (tempN - 49) + year + tempL
     // ------------------------------------------------------------------------
+    //solhint-disable max-line-length
     function _daysToDate(uint _days) internal pure returns (uint year, uint month, uint day) {
         int __days = int(_days);
 
-        int L = __days + 68569 + OFFSET19700101;
-        int N = 4 * L / 146097;
-        L = L - (146097 * N + 3) / 4;
-        int _year = 4000 * (L + 1) / 1461001;
-        L = L - 1461 * _year / 4 + 31;
-        int _month = 80 * L / 2447;
-        int _day = L - 2447 * _month / 80;
-        L = _month / 11;
-        _month = _month + 2 - 12 * L;
-        _year = 100 * (N - 49) + _year + L;
+        int tempL = __days + 68569 + OFFSET19700101;
+        int tempN = 4 * tempL / 146097;
+        tempL = tempL - (146097 * tempN + 3) / 4;
+        int _year = 4000 * (tempL + 1) / 1461001;
+        tempL = tempL - 1461 * _year / 4 + 31;
+        int _month = 80 * tempL / 2447;
+        int _day = tempL - 2447 * _month / 80;
+        tempL = _month / 11;
+        _month = _month + 2 - 12 * tempL;
+        _year = 100 * (tempN - 49) + _year + tempL;
 
         year = uint(_year);
         month = uint(_month);
