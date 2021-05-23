5008d67c618984eaa3034778573b1089e0f0a38d
Iztok Kavkler <iztok@abelium.com>
2021-05-23 06:45:01 +0000

    Resolve "Receiving FLR with WFLR"

---
+++ contracts/mock/implementations/TransferToWflrMock.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.7.6;
+
+import {IWFLR} from "../../userInterfaces/IWFLR.sol";
+
+/**
+ * @notice Provide a means to test behavior of WFLR when flares are transfered to it.
+ **/
+contract TransferToWflrMock {
+    receive() external payable {
+    }
+    
+    function transferToWflr(address payable wflr, uint256 amount) public {
+        wflr.transfer(amount);
+    }
+
+    function depositToWflr(address payable wflr, uint256 amount) public {
+        IWFLR(wflr).deposit{ value: amount }();
+    }
+}
