9d841a98ca598a6d944e8112781a00717d3431c3
Chuck Benedict <8324540-chuckb1@users.noreply.gitlab.com>
2021-06-21 21:28:31 +0000

    Resolve "Add transferGovernance to Governed"

--- test/unit/implementations/Governed.ts
+++ test/unit/implementations/Governed.ts
@@ -10,6 +10,7 @@ const ONLY_GOVERNANCE_MSG = "only governance";
 const NOT_CLAIMAINT = "not claimaint";
 
 const GOVERNANCEUPDATED_EVENT = "GovernanceUpdated";
+const GOVERNANCE_PROPOSED = "GovernanceProposed";
 
 contract(`Governed.sol; ${getTestFile(__filename)}; Governed unit tests`, async accounts => {
     // contains a fresh contract for each test
@@ -57,6 +58,15 @@ contract(`Governed.sol; ${getTestFile(__filename)}; Governed unit tests`, async 
             expectEvent.notEmitted(tx, GOVERNANCEUPDATED_EVENT);
         });
 
+        it("Should emit governance proposal event", async() => {
+          // Assemble
+          await governed.initialise(accounts[1]);
+          // Act
+          const tx = await governed.proposeGovernance(accounts[2], {from: accounts[1]});
+          // Assert
+          expectEvent(tx, GOVERNANCE_PROPOSED);
+        });
+
         it("Should reject a governance proposal if not proposed from governed address", async() => {
             // Assemble
             await governed.initialise(accounts[1]);
@@ -89,5 +99,49 @@ contract(`Governed.sol; ${getTestFile(__filename)}; Governed unit tests`, async 
             // Assert
             await expectRevert(claimPromise, NOT_CLAIMAINT);
         });
+
+        it("Should clear proposed address after claiming", async() => {
+            // Assemble
+            await governed.initialise(accounts[1]);
+            await governed.proposeGovernance(accounts[2], {from: accounts[1]});
+            // Act
+            await governed.claimGovernance({from: accounts[2]});
+            // Assert
+            const proposedAddress = await governed.proposedGovernance();
+            assert.equal(proposedAddress, constants.ZERO_ADDRESS);
+        });
+    });
+
+    describe("transfer", async() => {
+      it("Should transfer governance", async() => {
+        // Assemble
+        await governed.initialise(accounts[1]);
+        // Act
+        const tx = await governed.transferGovernance(accounts[2], {from: accounts[1]});
+        // Assert
+        const currentGovernance = await governed.governance();
+        assert.equal(currentGovernance, accounts[2]);
+        expectEvent(tx, GOVERNANCEUPDATED_EVENT);
+      });
+
+      it("Should reject transfer governance if not from governed address", async() => {
+        // Assemble
+        await governed.initialise(accounts[1]);
+        // Act
+        const promiseTransfer = governed.transferGovernance(accounts[2], {from: accounts[3]});
+        // Assert
+        await expectRevert(promiseTransfer, ONLY_GOVERNANCE_MSG);
+      });
+
+      it("Should clear proposed governance if sucessfully transferred", async() => {
+        // Assemble
+        await governed.initialise(accounts[1]);
+        await governed.proposeGovernance(accounts[2], {from: accounts[1]});
+        // Act
+        await governed.transferGovernance(accounts[3], {from: accounts[1]});
+        // Assert
+        const proposedGovernance = await governed.proposedGovernance();
+        assert.equal(proposedGovernance, constants.ZERO_ADDRESS);
+      });
     });
 });
