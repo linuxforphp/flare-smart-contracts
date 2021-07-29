11bc33bab2c9f4e55db96f762a4b2d0e1437f530
Chuck Benedict <chuck_benedict@yahoo.com>
2021-07-29 08:00:11 -0500

    Fixes #414

--- test/scdev/system/FlareKeeper.ts
+++ test/scdev/system/FlareKeeper.ts
@@ -19,16 +19,16 @@ contract(`FlareKeeper.sol; ${getTestFile(__filename)}; FlareKeeper system tests`
     let flareKeeper: FlareKeeperInstance;
     // contains a fresh contract for each test
     let contractToKeep: MockContractInstance;
-    let mintAccountingMock: MockContractInstance;
+    let inflationMock: MockContractInstance;
 
     before(async() => {
         // Defined in fba-avalanche/avalanchego/genesis/genesis_coston.go
         flareKeeper = await FlareKeeper.at("0x1000000000000000000000000000000000000002");
-        mintAccountingMock = await MockContract.new();
+        inflationMock = await MockContract.new();
         // Make sure keeper is initialized with a governance address...if may revert if already done.
         try {
             await flareKeeper.initialiseFixedAddress();
-            await flareKeeper.setMintAccounting(mintAccountingMock.address);
+            await flareKeeper.setInflation(inflationMock.address);
         } catch (e) {
             const governanceAddress = await flareKeeper.governance();
             if (genesisGovernance != governanceAddress) {
@@ -50,50 +50,21 @@ contract(`FlareKeeper.sol; ${getTestFile(__filename)}; FlareKeeper system tests`
           await flareKeeper.registerToKeep([{keptContract: contractToKeep.address, gasLimit: 0}], {from: genesisGovernance});
         });
 
-        afterEach(async() => {
-            await flareKeeper.unregisterAll({from: genesisGovernance});
-        });
-
         it("Should keep a contract", async() => {
             // Assemble
-            const startBlock = await flareKeeper.systemLastTriggeredAt();
+            const startInvocationCount = await contractToKeep.invocationCountForMethod.call(keep);
             // Act
             // Wait for some blocks to mine...
-            for(let i = 0; i < 5; i++) {
+            const blocks = 2;
+            for(let i = 0; i < blocks; i++) {
               await new Promise(resolve => {
                 setTimeout(resolve, 1000);
               });
               await advanceBlock();  
             }
             // Assert
-            const endBlock = await flareKeeper.systemLastTriggeredAt();
-            const invocationCount = await contractToKeep.invocationCountForMethod.call(keep);
-            assert(endBlock.sub(startBlock).eq(invocationCount));
-        });
-    });
-
-    describe("mint", async() => {
-        it ("Should cause the validator to mint", async() => {
-            // Assemble
-            const getMintingRequested = web3.utils.sha3("getMintingRequested()")!.slice(0,10); // first 4 bytes is function selector
-            // Rig up the mint accounting contract to tell the keeper to mint
-            await mintAccountingMock.givenMethodReturnUint(getMintingRequested, 1000);
-            const startBlock = await flareKeeper.systemLastTriggeredAt();
-            const openingBalance = BN(await web3.eth.getBalance(flareKeeper.address));
-            // Act
-            // Wait for some blocks to mine...
-            for(let i = 0; i < 5; i++) {
-                await new Promise(resolve => {
-                  setTimeout(resolve, 1000);
-                });
-                await advanceBlock();  
-            }
-            // Assert
-            // Balance in keeper should be the number of blocks mined times minting requested amount
-            const endBlock = await flareKeeper.systemLastTriggeredAt();
-            const closingBalance = BN(await web3.eth.getBalance(flareKeeper.address));
-            const shouldaMinted = BN(1000).mul((endBlock.sub(startBlock)));
-            assert(closingBalance.sub(openingBalance).eq(shouldaMinted));
+            const endInvocationCount = await contractToKeep.invocationCountForMethod.call(keep);
+            assert(endInvocationCount.sub(startInvocationCount).eq(BN(blocks)));
         });
     });
 });
