28cd7f5322b1d0b67eefe75079df96f834112a95
Iztok Kavkler <iztok@abelium.com>
2021-08-31 12:04:14 +0000

    daemonized contracts enter fallback mode on error (if supported)

--- contracts/genesis/interface/IFlareDaemonize.sol
+++ contracts/genesis/interface/IFlareDaemonize.sol
@@ -6,6 +6,14 @@ pragma solidity 0.7.6;
 ///     implement IFlareDaemonize
 interface IFlareDaemonize {
 
-    /// implement this function for recieving a trigger from FlareDaemon
-    function daemonize() external returns(bool);
+    /// Implement this function for recieving a trigger from FlareDaemon.
+    function daemonize() external returns (bool);
+    
+    /// This function will be called after an error is caught in daemonize().
+    /// It will switch the contract to a simpler fallback mode, which hopefully works when full mode doesn't.
+    /// Not every contract needs to support fallback mode (FtsoManager does), so this method may be empty.
+    /// Switching back to normal mode is left to the contract (typically a governed method call).
+    /// This function may be called due to low-gas error, so it shouldn't use more than ~30.000 gas.
+    /// @return true if switched to fallback mode, false if already in fallback mode or if falback not supported
+    function switchToFallbackMode() external returns (bool);
 }
