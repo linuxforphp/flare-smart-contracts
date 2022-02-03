901ff60aa439e267500c938d85a59ef40d2f0477
Iztok Kavkler <iztok@abelium.com>
2022-02-03 11:50:39 +0000

    support solidity versions 0.7 and 0.8 in interfaces

--- contracts/genesis/interface/IFtsoManagerGenesis.sol
+++ contracts/genesis/interface/IFtsoManagerGenesis.sol
@@ -1,6 +1,6 @@
 
 // SPDX-License-Identifier: MIT
-pragma solidity 0.7.6;
+pragma solidity >=0.7.6 <0.9;
 
 
 interface IFtsoManagerGenesis {
@@ -8,4 +8,3 @@ interface IFtsoManagerGenesis {
     function getCurrentPriceEpochId() external view returns (uint256 _priceEpochId);
 
 }
-
